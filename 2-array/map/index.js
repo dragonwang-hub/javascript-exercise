ec6a153ba68c114fea0a224c59e93f38734f9f2b
dragonwang-hub <915614879@qq.com>
2020-09-21 20:21:37 +0800

    [dragon] feat:finish 2-array-map

--- 2-array/map/index.js
+++ 2-array/map/index.js
@@ -1,3 +1,5 @@
 export default function numberMapToWord(collection) {
   // TODO 3: 在这里写实现代码
+  const result = collection.map(element => String.fromCharCode(96 + parseInt(element, 10)));
+  return result;
 }
