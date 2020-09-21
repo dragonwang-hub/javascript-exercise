f265da28d6dc517d208352d76cad5b6371386b29
dragonwang-hub <915614879@qq.com>
2020-09-21 20:14:34 +0800

    [dragon] feat: finished 2-array-find

--- 2-array/find/index.js
+++ 2-array/find/index.js
@@ -1,3 +1,5 @@
 export default function find00OldPerson(collection) {
   // TODO 4: 在这里写实现代码
+  const result = collection.find(element => element.age <= 20 && element.age >= 10);
+  return result.name;
 }
