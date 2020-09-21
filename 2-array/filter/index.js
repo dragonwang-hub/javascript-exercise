c5469042732916522d51bdc5bbdb618826939fa0
dragonwang-hub <915614879@qq.com>
2020-09-21 19:51:26 +0800

    [dragon] feat : finished 2-array-filter

--- 2-array/filter/index.js
+++ 2-array/filter/index.js
@@ -1,9 +1,15 @@
 function chooseMultiplesOfThree(collection) {
   // TODO 1: 在这里写实现代码
+  const result = collection.filter(element => element % 3 === 0);
+  return result;
 }
 
 function chooseNoRepeatNumber(collection) {
   // TODO 2: 在这里写实现代码
+  const result = collection.filter((word, index) => {
+    return collection.indexOf(word) === index;
+  });
+  return result;
 }
 
 export { chooseMultiplesOfThree, chooseNoRepeatNumber };
