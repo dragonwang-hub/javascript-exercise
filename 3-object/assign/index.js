1819a04db4a46fedbd78897c398ef7cb8c29bb75
dragonwang-hub <915614879@qq.com>
2020-09-21 20:46:06 +0800

    [dragon] feat: finished 3-object

--- 3-object/assign/index.js
+++ 3-object/assign/index.js
@@ -1,3 +1,9 @@
 export default function addSerialNumber(source) {
   // TODO 5: 在这里写实现代码，需要采用ES6 Object.assign
+  const status = { status: 'processed' };
+  Object.assign(source.properties, status);
+
+  const serialNumber = { serialNumber: '12345' };
+  Object.assign(source, serialNumber);
+  return source;
 }
--- 3-object/values/index.js
+++ 3-object/values/index.js
@@ -1,3 +1,6 @@
 export default function countTypesNumber(source) {
   // TODO 6: 在这里写实现代码
+  const values = Object.values(source);
+  const valusOfInt = values.map(value => Number(value));
+  return valusOfInt.reduce((accumulator, currentValue) => accumulator + currentValue);
 }
